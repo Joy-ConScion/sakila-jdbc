6f818c75f8f78350d4ae249acce6e897c130e61e
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-04 14:25:33 -0500

    Add code to match

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Instant;
 
 public class App {
 
@@ -16,19 +17,25 @@ public class App {
         dataSource.setUsername("root");
         dataSource.setPassword("");
 
-        String sql = "SELECT *,\n" +
-                "FROM actor;";
+        String sql = """
+                SELECT actor_id,
+                    first_name,
+                    last_name,
+                    last_update
+                FROM actor;
+                """;
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql);
              ResultSet resultSet = statement.executeQuery()) {
 
-            while (resultSet.next()) {
-             int actorId = resultSet.getInt("actor_id");
-             String firstName = resultSet.getString("first_name");
-             String lastName = resultSet.getString("last_name");
-             Instant lastUpdate = resultSet.getTimestamp("last_update");.toInstant
-             s
+            while(resultSet.next()){
+                int actorId = resultSet.getInt("actor_id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                Instant lastUpdate = resultSet.getTimestamp("last_update").toInstant();
+                System.out.printf(" %d %s %s %s", actorId, firstName, lastName, lastUpdate.toString());
+                System.out.println();
             }
 
             resultSet.close();

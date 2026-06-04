13a0ca9fd59da1561ebbf31d312d6671c9d5236e
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-04 14:22:25 -0500

    Add code for SQL with try/catch

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -2,6 +2,11 @@ package com.pluralsight;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class App {
 
     public static void main(String[] args) {
@@ -11,6 +16,29 @@ public class App {
         dataSource.setUsername("root");
         dataSource.setPassword("");
 
+        String sql = "SELECT *,\n" +
+                "FROM actor;";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+             int actorId = resultSet.getInt("actor_id");
+             String firstName = resultSet.getString("first_name");
+             String lastName = resultSet.getString("last_name");
+             Instant lastUpdate = resultSet.getTimestamp("last_update");.toInstant
+             s
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("Failed to retrieve request. Please contact your admin and try again.");
+            e.printStackTrace();
+        }
 
     }
 

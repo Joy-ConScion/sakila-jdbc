8866ebefac7195bbccd045f75a3b8eebc211208a
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-04 13:16:41 -0500

    Create & configure data source

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,10 +1,15 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class App {
 
     public static void main(String[] args) {
 
-
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
 
 
     }
